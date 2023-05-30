39bf2f5333edf5cd07a4c7fd58463da4758af145
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2023-05-30 20:46:22 +0700

    add material UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
 import Note from "./Notes";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Note />
+      <Footer />
     </div>
   );
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,23 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import List from "./List";
+import AddIcon from "@material-ui/icons/Add";
 
-function Notes() {
+function Note() {
+  const [isExpanded, setExpanded] = useState(false);
+  const [rows, setRows] = useState(1);
+
   const [notes, setNewNotes] = useState(null);
   const [formNote, setFormNote] = useState({
     title: "",
     content: "",
   });
 
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   function getNotes() {
     axios({
       method: "GET",
-      //   beacuse of the proxy in package.json
-      //we don't have to write "http:/localhost:8000/notes"
       url: "/notes/",
     })
       .then((response) => {
-        const data = response.data.setNewNotes(data);
+        const data = response.data;
+        setNewNotes(data);
       })
       .catch((error) => {
         if (error.response) {
@@ -36,9 +43,6 @@ function Notes() {
         title: formNote.title,
         content: formNote.content,
       },
-      // we use the response function to recall the getNotes()
-      // so that previous notes can be displayed together with the newly added note.
-      // we do this instead of making another function to call/update the entire data (refreshed using useEffect)
     }).then((response) => {
       getNotes();
     });
@@ -47,52 +51,59 @@ function Notes() {
       title: "",
       content: "",
     });
+    setExpanded(false);
     event.preventDefault();
   }
 
-  //like POST the response is used to update the notes after deleting an id
   function DeleteNote(id) {
     axios({
       method: "DELETE",
-      url: `notes/${id}`,
+      url: `/notes/${id}/`,
     }).then((response) => {
       getNotes();
     });
   }
 
-  useEffect(() => {
-    getNotes();
-    // empty [] array is pass as the second argument
-    // to prevent the function from running in an infinite loop
-  }, []);
-
   function handleChange(event) {
     const { value, name } = event.target;
-    // added new name & value to the note
     setFormNote((prevNote) => ({
       ...prevNote,
       [name]: value,
     }));
   }
 
+  function NoteShow() {
+    setExpanded(true);
+    setRows(3);
+  }
+
   return (
     <div className="">
       <form className="create-note">
-        <input
-          onChange={handleChange}
-          text={formNote.title}
-          name="title"
-          placeholder="Title"
-          value={formNote.title}
-        />
+        {isExpanded && (
+          <input
+            onChange={handleChange}
+            text={formNote.title}
+            name="title"
+            placeholder="Title"
+            value={formNote.title}
+          />
+        )}
         <textarea
+          onClick={NoteShow}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
+          rows={rows}
           value={formNote.content}
         />
-        <button onClick={createNote}>Create Post</button>
+        {isExpanded && (
+          <button onClick={createNote}>
+            <AddIcon />
+          </button>
+        )}
       </form>
+
       {notes &&
         notes.map((note) => (
           <List
@@ -106,4 +117,5 @@ function Notes() {
     </div>
   );
 }
-export default Notes;
+
+export default Note;
